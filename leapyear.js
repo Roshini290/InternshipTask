9b38c6befc02ee967e5b6b822d445c49780d9f43
Roshini290 <100557461+Roshini290@users.noreply.github.com>
2022-03-18 14:51:30 +0530

    Update leapyear.js

--- leapyear.js
+++ leapyear.js
@@ -1,10 +1,10 @@
-// program to check leap year or not
-function checkLeapYear(year) {
-
-/*  three conditions to find out the leap year
+/* program to check leap year or not
+    three conditions to find out the leap year
     Return true if year is a multiple
     of 4 and not multiple of 100.
-    OR year is multiple of 400.*/
+    OR year is multiple of 400. */
+
+function checkLeapYear(year) {
    if((year % 400 == 0) || (year % 100 != 0) && (year % 4 == 0)) {
      return true;
    } else {
@@ -12,7 +12,6 @@ function checkLeapYear(year) {
    }
 }
 
-//variable declaration and function calling
 let year = 2020;
 console.log( checkLeapYear(year));
 
