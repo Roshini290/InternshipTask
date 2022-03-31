36157cd76cb4f2f442bd614e5deee8e44f3f1242
Roshini290 <100557461+Roshini290@users.noreply.github.com>
2022-03-31 15:09:41 +0530

    init

--- perimeter.js
+++ perimeter.js
@@ -10,7 +10,7 @@ function findPerimeter(length, width) {
     return perimeter;
 }
 
-let length = 2;
+let length = 3;
 let width = 3;
 let result = findPerimeter(length, width);
 console.log(" Perimeter of the Rectangle = ", result);
