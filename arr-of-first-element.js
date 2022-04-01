670117b6adba3ad7106c4b3d9647adc5dec8d2f1
Roshini290 <100557461+Roshini290@users.noreply.github.com>
2022-04-01 18:09:13 +0530

    Update arr-of-first-element.js

--- arr-of-first-element.js
+++ arr-of-first-element.js
@@ -1,13 +1,13 @@
 /** 
- * @param {object} a - array
- * @return {object} firstIndex - returns the first element of an array
+ * Returns the first element in an array
+ * @param {object} arr - mixed array
+ * @returns {object} firstIndex - returns the first element of mixed array
  */
 
- function arrOfFirst(a) {
-    var firstIndex = a[0]; 
+ function arrOfFirst(arr) {
+    var firstIndex = arr[0]; 
     return firstIndex;  
 }
 
 let result = arrOfFirst([ "Hello Javascript", 2.5, 3, 4, 5 ]);
 console.log(" First element in the array = ", result);
-
