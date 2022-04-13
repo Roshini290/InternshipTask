eb319cdf6b94ef8d822afd9e9888c8bbed99249b
Roshini290 <100557461+Roshini290@users.noreply.github.com>
2022-04-13 12:03:00 +0530

    Update sum-of-polygon-angles.js

--- sum-of-polygon-angles.js
+++ sum-of-polygon-angles.js
@@ -1,9 +1,3 @@
-/**
- * Returns sum of the internal angles of a ploygon
- * @param {number} sides - number of sides in a polygon
- * @returns {number} - returns sum of internal angles of a polygon
- */
-
 function sumOfInternalAngles(sides) {
 
     if (sides < 3) {
